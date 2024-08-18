d855aa24eb4f6db45aef19a57ee4860e168b67eb
natt <natt.korat@cadt.edu.kh>
2024-08-18 11:31:32 +0700

    finish prototype

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import utils
 window = display.set_mode((600, 400))
 display.set_caption("Ping Pong Game")
 
+
 icon = image.load("ball.png")
 display.set_icon(icon)
 
@@ -11,18 +12,54 @@ clock = time.Clock()
 
 background = (200, 255, 255)
 
-ball = utils.GameSprite("ball.png", 300, 200, 50, 50, 3)
+ball = utils.Ball("ball.png", 300, 200, 30, 30, 3)
+
+l_paddle = utils.Paddle(20, 150, 20, 100, (255, 255, 0))
+r_paddle = utils.Paddle(540, 150, 20, 100, (255, 255, 0))
+
+font.init()
+fonts = font.Font(None, 70)
+lose1 = fonts.render(
+    "PLAYER 1 LOSE",
+    True,
+    (200, 50, 20)
+)
+lose2 = fonts.render(
+    "PLAYER 2 LOSE",
+    True,
+    (200, 50, 200)
+)
 
 
 game = True
+finish = False
+
 while game:
-    window.fill(background)
 
     for e in event.get():
         if e.type == QUIT:
             game = False
-    
-    ball.reset(window)
+
+    if not finish:
+        window.fill(background)
+        ball.reset(window)
+        l_paddle.reset(window)
+        r_paddle.reset(window)
+
+        ball.update(window)
+        ball.is_collided(r_paddle)
+        ball.is_collided(l_paddle)
+
+        l_paddle.l_update(window)
+        r_paddle.r_update(window)
+        
+        if ball.rect.x < l_paddle.rect.x:
+            finish = True
+            window.blit(lose1, (130, 150))
+        if ball.rect.x > r_paddle.rect.x:
+            finish = True
+            window.blit(lose2, (130, 150))
+
 
     clock.tick(60)
     display.update()
--- utils.py
+++ utils.py
@@ -20,18 +20,52 @@ class GameSprite(pygame.sprite.Sprite):
     def reset(self, window):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-class Player(GameSprite):
-    bullets = pygame.sprite.Group()
+class Ball(GameSprite):
+    def __init__(self, image, x, y, w, h, speed=0):
+        super().__init__(image, x, y, w, h, speed)
+        self.speed_x = self.speed
+        self.speed_y = self.speed
+        self.h = h
+        self.w = w
 
-    def update(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and self.rect.x > 0:
-            self.rect.x -= self.speed
-        if keys[pygame.K_RIGHT] and self.rect.x < 630:
-            self.rect.x += self.speed
+    def update(self, window: pygame.surface):
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
         
-        if keys[pygame.K_SPACE]:
-            self.fire()
-            fire_sound = pygame.mixer.Sound("fire.ogg")
-            fire_sound.set_volume(0.15)
-            fire_sound.play()
+        if self.rect.y > window.get_size()[1] - self.h or self.rect.y <= 0:
+            self.speed_y *= -1
+        if self.rect.x > window.get_size()[0] - self.w or self.rect.x <= 0:
+            self.speed_x *= -1
+    
+    def is_collided(self, object: pygame.Surface):
+        if pygame.sprite.collide_rect(self, object):
+            self.speed_x *= -1
+
+
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self, x, y, w, h, color):
+        super().__init__()
+        self.image = pygame.Surface((w, h))
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.w = w
+        self.h = h
+    
+    def reset(self, window):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+    
+    def l_update(self, window: pygame.Surface):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w] and self.rect.y > 0:
+            self.rect.y -= 5
+        if keys[pygame.K_s] and self.rect.y < window.get_size()[1] - self.h:
+            self.rect.y += 5
+    
+    def r_update(self, window: pygame.Surface):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and self.rect.y > 0:
+            self.rect.y -= 5
+        if keys[pygame.K_DOWN] and self.rect.y < window.get_size()[1] - self.h:
+            self.rect.y += 5
